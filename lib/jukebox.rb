faa534655106ad885944b1a39459bd975ff59532
Mariamol James <mariamol.james@gmail.com>
2020-06-21 15:03:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -47,7 +47,7 @@ def run(songs)
   user_command=gets.strip
   if not user_command.eql?("exit")
     if user_command.eql("help")
-      
+
     elsif user_command.eql("list")
       list(songs)
     else
