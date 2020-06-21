52281cb279ccd6fb5a361d8c0426937e4b769ea2
Mariamol James <mariamol.james@gmail.com>
2020-06-21 15:08:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,7 +19,7 @@ def help
 end
 
 def list(songs)
-  songs.each_with_index {|song,index| puts "#{index+1}. song"}
+  songs.each_with_index {|song,index| puts "#{index+1}. #{song}"}
 end
 
 def play(songs)
